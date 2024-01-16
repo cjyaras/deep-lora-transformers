3c06c761d978e11b5639cb51253c450e7b8d8f93
cjyaras <cjyaras@gmail.com>
2024-01-16 16:50:30 -0500

    changed default configs

--- configs.py
+++ configs.py
@@ -6,15 +6,14 @@ from data import task_to_keys
 
 @dataclass
 class ModelArguments:
-    # pretrain_model: str = "bert-base-cased"
-    pretrain_model: str = "roberta-base"
+    pretrain_model: str = "bert-base-cased"  # = "roberta-base"
 
 
 @dataclass
 class DataArguments:
     finetune_task: str = "stsb"
     max_seq_length: int = 32
-    max_train_samples: Optional[int] = 8
+    max_train_samples: Optional[int] = 4
 
     def __post_init__(self):
         assert self.finetune_task in task_to_keys.keys()
@@ -25,8 +24,8 @@ class TrainArguments:
     finetune_strategy: str = "lora2"
     seed: int = 1
     num_train_epochs: int = 2000
-    per_device_train_batch_size: int = 1
-    per_device_eval_batch_size: int = 4
+    per_device_train_batch_size: int = 4
+    per_device_eval_batch_size: int = 32
     warmup_steps: int = 0
     learning_rate: float = 2e-5
     weight_decay: float = 0.0
